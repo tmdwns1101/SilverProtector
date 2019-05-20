093e23767d0008200eb3b22bf4d65c76e6b91b51
DESKTOP-SAJN8LR\PC <tmdwns1101@naver.com>
2019-05-20 16:18:59 +0900

    기능 추가
    기능 :
    1. 사용자가 외출시 DB로 외출 날짜 및 시간 업데이트
    2. 사용자 외출 감지 기능 구현

--- FallDownDetect/MotionDetect.py
+++ FallDownDetect/MotionDetect.py
@@ -1,13 +1,15 @@
+
+from devieceinfo.DeviceInfoDAO import *
+from userinfo.UserInfoDAO import *
+from userOuting.UserOutingDAO import *
 import cv2
-import threading
 import numpy as np
 import time
 from imutils.video import VideoStream
 import argparse
 import datetime
 import imutils
-from devieceinfo.DeviceInfoDAO import *
-from userinfo.UserInfoDAO import *
+from datetime import datetime
 
 
 class MotionDetect:
@@ -18,6 +20,7 @@ class MotionDetect:
     ap.add_argument("-a", "--min-area", type=int, default=2000, help="minimum area size")  # defualt= 500
     args = vars(ap.parse_args())
     noObjectFlag = False
+    outingFlag = False    #사용자가 집에 나갔을 때 True로 바뀜
 
     def __init__(self, cam, deviceID):
         self.ret, self.frame = cam.read()
@@ -44,17 +47,28 @@ class MotionDetect:
             vs = VideoStream(src=0).start()
             time.sleep(2.0)
 
+            # 0503 Moving Average Background Subtraction
+            frame = vs.read()
+            avg1 = np.float32(frame)
+
         # otherwise, we are reading from a video file
         else:
             vs = cv2.VideoCapture(self.args["video"])
+            # 0503 Moving Average Background Subtraction
+            frame = vs.read()
+            avg1 = np.float32(frame)
 
         # initialize the first frame in the video stream
         firstFrame = None
         print(self.userID)
 
+        ang1 = -100
+
         # loop over the frames of the video
         while True:
             #print(self.fallDownCheck)
+
+
 
 
             # grab the current frame and initialize the occupied/unoccupied
@@ -62,6 +76,10 @@ class MotionDetect:
             frame = vs.read()
             frame = frame if self.args.get("video", None) is None else frame[1]
             text = "Unoccupied"
+
+            # 0503 Moving Average Background Subtraction
+            cv2.accumulateWeighted(frame, avg1, 0.1)
+            res1 = cv2.convertScaleAbs(avg1)
 
             if self.noObjectFlag is False:
                 start = time.time()
@@ -71,6 +89,20 @@ class MotionDetect:
             if end - start >= 10 and self.noObjectFlag is True:
                 print("Object MiSS!!!")
                 self.userDAO.UpdateUserMiss(userID=self.userID, state=1)
+
+            #Change 2019/05/15 for 외출 시간 전송
+            if self.outingFlag is False:
+
+                outingStart = time.time()
+                self.outingFlag = True
+            outingEnd = time.time()
+
+            if outingEnd - outingStart >= 600 and self.outingFlag is True:
+                UserOutingDAO().insertDate(userID=self.userID)
+
+
+
+
 
             # if the frame could not be grabbed, then we have reached the end
             # of the video
@@ -88,13 +120,15 @@ class MotionDetect:
 
             # compute the absolute difference between the current frame and
             # first frame
+            # thresh changed 25 -> 75 because of light dilation
             frameDelta = cv2.absdiff(firstFrame, gray)
-            thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.threshold(frameDelta, 75, 255, cv2.THRESH_BINARY)[1]
 
             # dilate the thresholded image to fill in holes, then find contours
             # on thresholded image
             thresh = cv2.dilate(thresh, None, iterations=2)
-            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            #cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
             cnts = imutils.grab_contours(cnts)
 
             # loop over the contours
@@ -115,33 +149,48 @@ class MotionDetect:
                 # and update the text
                 (x, y, w, h) = cv2.boundingRect(c)
                 #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                #Rotated Rectangle
+                rect = cv2.minAreaRect(c)
+                ((x1, y1), (x2, y2), ang) = cv2.minAreaRect(c)
+                box = cv2.boxPoints(rect)
+                box = box.astype('int')
+                cv2.drawContours(frame, [box], -1, 7)  # blue
+
                 if self.noObjectFlag is True:
                     self.userDAO.UpdateUserMiss(userID=self.userID, state=0)
 
                 self.noObjectFlag = False
-
-
+                self.outingFlag = False    #외출 상태 초기화
 
                 text = "Occupied"
-                # test1
+
+                # user status : sitting / standing / laying
                 if h > 1.3 * w:
                     str = "standing"
+                    ang1 = int(float(ang))
+                    time.sleep(0.5)
                     self.userDAO.UpdateUserFallDown(userID=self.userID, state=0)
                     self.fallDownCheck = False
                 else:
                     if w > 1.3 * h:
                         str = "laying"
-                        if self.fallDownCheck is False:
+
+                        ang2 = int(float(ang))
+                        sub_ang = ang1 - ang2
+                        print(self.fallDownCheck," ",ang2, ang1, sub_ang)
+
+                        if self.fallDownCheck is False and sub_ang > 20:
                             self.fallDownCheck = True
                             fallstart = time.time()
-                            #mythread = threading.Thread(target=self.Timer, args=(5, ))
-                            #mythread.daemon = True
-                            #mythread.start()
-                        fallend = time.time()
-                        if fallend - fallstart >= 5:
-                            print("Warning!")
-                            self.fallDownCheck = False
-                            self.userDAO.UpdateUserFallDown(userID=self.userID, state=1)
+                            print("falldown check start",sub_ang)
+
+                        if self.fallDownCheck is True:
+                            fallend = time.time()
+                            if fallend - fallstart >= 5:
+                                print("Warning!")
+                                self.fallDownCheck = False
+                                self.userDAO.UpdateUserFallDown(userID=self.userID, state=1)
 
                     else:
                         if w:
@@ -159,12 +208,15 @@ class MotionDetect:
             # draw the text and timestamp on the frame
             cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-            cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+            #cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+            #            cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
             # show the frame and record if the user presses a key
             cv2.imshow("Security Feed", frame)
             cv2.imshow("Thresh", thresh)
             cv2.imshow("Frame Delta", frameDelta)
+            # 0503 Moving Average Background Subtraction
+            cv2.imshow('avg1', res1)
+
             key = cv2.waitKey(1) & 0xFF
             # if the `q` key is pressed, break from the lop
             if key == ord("q"):
