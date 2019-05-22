9952d5dd1ec3e62d1c9324a124264958f0849b2d
재원 서 <novice1995@gmail.com>
2019-05-23 01:05:31 +0900

    version 1.4.1 (5/23) use histogram equalization after gaussian blur upgrade fall down algorithm (angle -> state change) change delite iteration 4
    version 1.4.0 (5/20)
    add outing status value outingFlag and package userOuting
    
    version 1.3.2
    rotated rectangle : more than 50 angle while standing to laying falldown check start
    thread fix 25 to 75
    MySQL localhost -> school ip address
    add Moving Average Background Subtraction
    
    version 1.2.2
    use mysql+Serial number Read

--- JW/Motion_Detect/motion_detect.py
+++ JW/Motion_Detect/motion_detect.py
@@ -1,58 +1,42 @@
 import cv2
-import threading
 import numpy as np
 import time
 from imutils.video import VideoStream
 import argparse
 import datetime
 import imutils
+import serial
+
 from JW.deviceinfo.DeviceInfoDAO import *
 from JW.userinfo.UserInfoDAO import *
 from JW.userOuting.UserOutingDAO import *
 
 class MotionDetect:
 
+    #ser = serial.Serial(port="COM12", baudrate=9600)
+
     ap = argparse.ArgumentParser()
     ap.add_argument("-v", "--video", help="path to the video file")
-    ap.add_argument("-a", "--min-area", type=int, default=2000, help="minimum area size")  # defualt= 500
+    ap.add_argument("-a", "--min-area", type=int, default=10000, help="minimum area size")  # defualt= 500
     args = vars(ap.parse_args())
-    noObjectFlag = False
-    fallDownCheck = False
-    outingFlag = False
 
     def __init__(self, cam, deviceID):
         self.ret, self.frame = cam.read()
+        # data for mySQL
         self.deviceID = deviceID
         self.deviceDAO = DeviceInfoDAO()
         self.userID = self.deviceDAO.getUserID(self.deviceID)
         self.userDAO = UserInfoDAO()
 
-    def Timer(self, limit):
-        start = time.time()
-        while True:
-            end = time.time()
-            if end - start >= limit:
-                print("Warning!")
-                self.fallDownCheck = False
-                self.userDAO.UpdateUserFallDown(userID=self.userID, state=1)
-
-                break
-
     def ObjectDetector(self):
         # if the video argument is None, then we are reading from webcam
-
         if self.args.get("video", None) is None:
             vs = VideoStream(src=0).start()
             time.sleep(2.0)
 
-            # 0503 Moving Average Background Subtraction
-            frame = vs.read()
-            avg1 = np.float32(frame)
-
         # otherwise, we are reading from a video file
         else:
             vs = cv2.VideoCapture(self.args["video"])
-            # 0503 Moving Average Background Subtraction
             frame = vs.read()
             avg1 = np.float32(frame)
 
@@ -60,40 +44,49 @@ class MotionDetect:
         firstFrame = None
         print(self.userID)
 
-        ang1 = -100
+        # initialize count, state and flag values
+        standCount = 0
+        sittingCount = 0
+        curState = "No"
+        noObjectFlag = False
+        fallDownCheck = False
+        outingFlag = False  # if user is not in home
+        indoorFlag = True  # if user is in home
+        outingCurState = False  # current user state
+        outingPreState = False  # last user state
 
         # loop over the frames of the video
         while True:
-            #print(self.fallDownCheck)
-
-
-            # grab the current frame and initialize the occupied/unoccupied
-            # text
+            # grab the current frame and initialize the occupied/unoccupied text
             frame = vs.read()
             frame = frame if self.args.get("video", None) is None else frame[1]
             text = "Unoccupied"
 
-            # 0503 Moving Average Background Subtraction
-            cv2.accumulateWeighted(frame, avg1, 0.1)
-            res1 = cv2.convertScaleAbs(avg1)
-
+            # if user is not at home, save start time and make noObjectFlag true
             if self.noObjectFlag is False:
                 start = time.time()
                 self.noObjectFlag = True
+            # set end time to current time
             end = time.time()
 
-            if end - start >= 10 and self.noObjectFlag is True:
+            # if difference between end and start is larger than 8 hours
+            # and noObjectFlag is True update mySQL UserMiss
+            if end - start >= 3600*8 and self.noObjectFlag is True:
                 print("Object MiSS!!!")
                 self.userDAO.UpdateUserMiss(userID=self.userID, state=1)
 
-            # Change 2019/05/15 for 외출 시간 전송
+            # time to check outing pattern
             if self.outingFlag is False:
                 outingStart = time.time()
                 self.outingFlag = True
             outingEnd = time.time()
 
+            # if user is missing for 10 minutes and outingFlag is True
+            # and indoorFlag is Ture update mySQL insertDate
             if outingEnd - outingStart >= 600 and self.outingFlag is True:
-                UserOutingDAO().insertDate(userID=self.userID)
+                if self.indoorFlag is True:
+                    UserOutingDAO().insertDate(userID=self.userID)
+                    self.indoorFlag = False
 
             # if the frame could not be grabbed, then we have reached the end
             # of the video
@@ -101,9 +94,12 @@ class MotionDetect:
                 break
 
             # resize the frame, convert it to grayscale, and blur it
+            # for light smear problem, use histogram equalization
             frame = imutils.resize(frame, width=500)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             gray = cv2.GaussianBlur(gray, (21, 21), 0)
+            gray = cv2.equalizeHist(gray)
+
             # if the first frame is None, initialize it
             if firstFrame is None:
                 firstFrame = gray
@@ -111,102 +107,119 @@ class MotionDetect:
 
             # compute the absolute difference between the current frame and
             # first frame
-            # thresh changed 25 -> 75 because of light dilation
             frameDelta = cv2.absdiff(firstFrame, gray)
-            thresh = cv2.threshold(frameDelta, 75, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.threshold(frameDelta, 60, 255, cv2.THRESH_BINARY)[1]
 
             # dilate the thresholded image to fill in holes, then find contours
             # on thresholded image
-            thresh = cv2.dilate(thresh, None, iterations=2)
-            #cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            thresh = cv2.dilate(thresh, None, iterations=4)
             cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
             cnts = imutils.grab_contours(cnts)
 
+            maxValue = 0
+            maxIdx = 0
             # loop over the contours
+            # find max area value in contours
             if cnts:
                 areas = []
-                for c in cnts:
+                for i in range(0, len(cnts)):
+                    c = cnts[i]
                     ar = cv2.contourArea(c)
-                    areas.append(ar)
-
+                    if maxValue < ar:
+                        maxValue = ar
+                        maxIdx = i
+                areas.append(ar)
                 max_area = max(areas or [0])
-                max_area_index = areas.index(max_area)
-                maxCnt = cnts[max_area_index]
+
                 # if the contour is too small, ignore it
                 if max_area < self.args["min_area"]:
                     continue
 
                 # compute the bounding box for the contour, draw it on the frame,
-                # and update the text
-                (x, y, w, h) = cv2.boundingRect(c)
-                #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                (x, y, w, h) = cv2.boundingRect(cnts[maxIdx])
 
-                #Rotated Rectangle
-                rect = cv2.minAreaRect(c)
-                ((x1, y1), (x2, y2), ang) = cv2.minAreaRect(c)
-                box = cv2.boxPoints(rect)
-                box = box.astype('int')
-                cv2.drawContours(frame, [box], -1, 7)  # blue
-
-                if self.noObjectFlag is True:
-                    self.userDAO.UpdateUserMiss(userID=self.userID, state=0)
-
-                self.noObjectFlag = False
-                self.outingFlag = False # 외출상태 초기화
-
+                # contours is capturing something, update text and state
+                # reset noObject, outing, indoor flag and str
                 text = "Occupied"
+                preState = curState
+                print("current state :")
+                print(preState)
+                str = "null"
+                self.noObjectFlag = False
+                self.outingFlag = False
+                self.indoorFlag = True
 
-                # user status : sitting / standing / laying
+                # user status : sitting / standing / Fall down / sleep
+                # accumulate stand count and if stand count is more than
+                # 100, update current state
+                # reset fallDownCheck and mySQL UserFallDown
                 if h > 1.3 * w:
                     str = "standing"
-                    ang1 = int(float(ang))
-                    time.sleep(0.5)
+                    standCount += 1
+                    sittingCount = 0
+                    print("Stand Count :", standCount)
+                    if standCount >= 100:
+                        curState = "standing"
                     self.userDAO.UpdateUserFallDown(userID=self.userID, state=0)
                     self.fallDownCheck = False
-                else:
-                    if w > 1.3 * h:
-                        str = "laying"
 
-                        ang2 = int(float(ang))
-                        sub_ang = ang1 - ang2
-                        print(self.fallDownCheck," ",ang2, ang1, sub_ang)
+                # if wide is longer than height and preState is standing
+                elif w > 1.3 * h:
+                    if preState != "No" and preState == "standing":
+                        str = "Fall Down"
 
-                        if self.fallDownCheck is False and sub_ang > 20:
-                            self.fallDownCheck = True
-                            fallstart = time.time()
-                            print("falldown check start",sub_ang)
-
-                        if self.fallDownCheck is True:
-                            fallend = time.time()
-                            if fallend - fallstart >= 5:
-                                print("Warning!")
-                                self.fallDownCheck = False
-                                self.userDAO.UpdateUserFallDown(userID=self.userID, state=1)
-
-                    else:
-                        if w:
-                            self.userDAO.UpdateUserFallDown(userID=self.userID, state=0)
+                    # measure time and if user is laying more than 5 sec
+                    # update fallDownCheck and mySQl UserFalldown
+                    if self.fallDownCheck is False:
+                        self.fallDownCheck = True
+                        fallstart = time.time()
+                    if self.fallDownCheck is True:
+                        fallend = time.time()
+                        if fallend - fallstart >= 5:
+                            print("Warning!")
                             self.fallDownCheck = False
-                            str = "sitting"
+                            self.userDAO.UpdateUserFallDown(userID=self.userID, state=1)
+
+                    # if wide is longer than height and preState is sitting
+                    # set str sleep
+                    elif preState == "sitting":
+                        str = "sleep"
+
+                # if wide and height are similar
+                # reset fallDownCheck and mySQL UserFallDown
+                elif h <= 1.3 * w and w <= 1.3 * h:
+                    self.userDAO.UpdateUserFallDown(userID=self.userID, state=0)
+                    self.fallDownCheck = False
+                    str = "sitting"
+
+                    # accumulate stand count and if sitting count is more than
+                    # 100, update current state
+                    standCount = 0
+                    sittingCount += 1
+                    if sittingCount >= 100:
+                        curState = "sitting"
+                    print("Sitting Count :", sittingCount)
+
+                # make frame for different color and put text str
                 if str is "standing":
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 if str is "sitting":
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                if str is "laying":
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
+                if str is "Fall Down":
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                if str is "sleep":
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
                 cv2.putText(frame, str, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
             # draw the text and timestamp on the frame
             cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
             cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
             # show the frame and record if the user presses a key
             cv2.imshow("Security Feed", frame)
             cv2.imshow("Thresh", thresh)
             cv2.imshow("Frame Delta", frameDelta)
-            # 0503 Moving Average Background Subtraction
-            cv2.imshow('avg1', res1)
 
             key = cv2.waitKey(1) & 0xFF
             # if the `q` key is pressed, break from the lop
